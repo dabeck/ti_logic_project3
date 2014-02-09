f00efaff3e5417f389b90c46f3ec77d3b014eb28
flexxor <choleriker99@gmail.com>
2014-02-09 11:22:47 +0100

    SkomenToKNF

---
+++ src/de/unikassel/ti/logic/project3/converters/SkolemToKnfConverter.java
@@ -0,0 +1,15 @@
+package de.unikassel.ti.logic.project3.converters;
+
+import de.unikassel.ti.logic.project3.model.Formula;
+
+public class SkolemToKnfConverter {
+
+	public static Formula convert(Formula transformedToSkolemNF) {
+		
+		
+		
+		
+		return null;
+	}
+
+}
