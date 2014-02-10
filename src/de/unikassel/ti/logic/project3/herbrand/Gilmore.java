df88b1008a7d68518ddc5c827e70f9623e62750f
flexxor <choleriker99@gmail.com>
2014-02-10 17:38:36 +0100

    was noch fehlt in 'Gilmore' ergaenzt

--- src/de/unikassel/ti/logic/project3/herbrand/Gilmore.java
+++ src/de/unikassel/ti/logic/project3/herbrand/Gilmore.java
@@ -1,5 +1,37 @@
 package de.unikassel.ti.logic.project3.herbrand;
 
+import org.sat4j.core.VecInt;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.IVecInt;
+
 public class Gilmore {
 
+	private static ISolver solver = SolverFactory.newDefault();
+	
+	// TODO: list of clauses
+		
+	
+	// TODO: erzeuge Mapping von prädikatenlogischen Formeln auf aussagenlogische
+	
+	// TODO: umwandeln der Formel, die HerbrandExpander hergibt, in Klauseln
+	
+	
+	
+	
+	public static void test() {
+		
+		int[] lits = null;
+		VecInt arg0 = new VecInt(lits); 
+		try {
+			solver.addClause(arg0);
+		} catch (ContradictionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 }
