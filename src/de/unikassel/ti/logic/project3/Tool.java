0f3960845bb4367b41b61ac42a2a7d099518a4d6
muellerp <mw@live.de>
2014-02-10 16:44:48 +0100

    Tool: Write the CORRECT formula in the console!!

--- src/de/unikassel/ti/logic/project3/Tool.java
+++ src/de/unikassel/ti/logic/project3/Tool.java
@@ -15,7 +15,8 @@ public class Tool {
     public static void main(String[] args) {
 
 	    // parser p = new parser(new Scanner(new InputStreamReader(System.in)));
-	    parser p = new parser(new Scanner(new StringReader("- EXISTS x ( R(f(x), g(g(c,x))) & -P(f(f(d))))")));
+	    parser p = new parser(new Scanner(new StringReader("- EXISTS x ( R(f(x), g(h(c,x))) & -P(i(j(d))))")));
+    	//parser p = new parser(new Scanner(new StringReader("EXISTS x ( FORALL y ( EXISTS z ( FORALL u ( EXISTS v ( EXISTS w ( P(x, y, z, u, v, w) ) ) ) ) ) )")));
 	    Formula f = null;
 	    
 	    
@@ -26,21 +27,21 @@ public class Tool {
 
 		    // 2. convert formula to PNF. Has to return a formula
 		    Formula transformedToPositiveNF = f.transformToPositiveNF();
-		    System.out.println("\nFormula in postive normal form:\n" + f.toString());
+		    System.out.println("\nFormula in postive normal form:\n" + transformedToPositiveNF.toString());
 		    
 		    // 3. convert formula to prenex normal form. Has to return a formula
 		    Formula transformedToPrenexNF = PrenexConverter.convert(transformedToPositiveNF);		    
-		    System.out.println("\nFormula in prenex normal form:\n" + f.toString());
+		    System.out.println("\nFormula in prenex normal form:\n" + transformedToPrenexNF.toString());
 		    
 		    // 4. convert formula to Skolem normal form. Has to return a formula
-		    SkolemConverter.setVariables(PrenexConverter.getVariableList());
+		    //SkolemConverter.setVariables(PrenexConverter.getVariableList());
 		    Formula transformedToSkolemNF = SkolemConverter.convert(transformedToPrenexNF);
 		    
-		    System.out.println("\nFormula in Skolem normal form:\n" + f.toString());
+		    System.out.println("\nFormula in Skolem normal form:\n" + transformedToSkolemNF.toString());
 		    
 		    // 5. Transform to KNF
 		    //Formula transformedToKnf = SkolemToCNFConverter.convert(transformedToSkolemNF);
-		    System.out.println("\nFormula in KNF:\n" + f.toString());
+		    //System.out.println("\nFormula in KNF:\n" + transformedToKnf.toString());
 		    
 		    // 6. Herbrand Expansion and Gilmore
 		    ArrayList<Formula> formulasExpanded = new ArrayList<Formula>();
@@ -48,7 +49,7 @@ public class Tool {
 		    HerbrandExpander he = new HerbrandExpander(transformedToSkolemNF);
 		    Gilmore gilmore = new Gilmore();
 		    
-		    while (true/*he.hasNext()*/) {
+		    while (he.hasNext()) {
 		    	Formula next = he.getNext();
 		    	formulasExpanded.add(next);
 		    }
